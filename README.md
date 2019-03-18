409856c90b951c5b8dfdfd5fa1bc6957ea362d61
Geert Gerritsen <geertgerritsen@gmail.com>
2019-03-18 16:05:23 +0100

    Used first building block (postgres) in app

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # building-blocks-app
 App created from building blocks
+
+## How to include a building block
+1. Clone to a directory:
+e.g. `git clone https://github.com/ggerritsen/postgresql-block repository`
+2. Update the package name of the building block files to reflect the new path
+3. Use the building block's code in the app and edit it where necessary
--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/ggerritsen/building-blocks-app/repository"
+)
 
 func main() {
 	println(fmt.Sprintf("Hello, %s", "hoi"))
+
+	repo, err := repository.NewRepositoryWithDb("localhost", "postgres", "your-password", "template1", 5432)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer repo.Close()
+	fmt.Printf("Connected to database\n")
 }
