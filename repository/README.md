b1f3211a58e6c20f3f82a943bb436f889f2dbb7c
Geert Gerritsen <geertgerritsen@gmail.com>
2019-03-18 15:47:55 +0100

    Add building block github.com/ggerritsen/postgresql-block version 0.1

---
+++ repository/README.md
@@ -0,0 +1,6 @@
+# postgresql-block 
+Building block for postgresql connections
+
+## To do
+- use go modules for dependency
+- use context-aware errors (see https://dave.cheney.net/practical-go/presentations/qcon-china.html#_wrapping_errors_with_github_compkgerrors)
---
+++ repository/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
+func main() {
+	fmt.Printf("Start of demonstration.\n")
+
+	repo, err := NewRepositoryWithDb("localhost", "postgres", "your-password", "template1", 5432)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer repo.Close()
+	fmt.Printf("Connected to database\n")
+
+	if err := repo.CreateTable(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Table created\n")
+
+	recordName := "testRecord"
+	id, err := repo.Insert(recordName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Inserted record with id %d\n", id)
+
+	record, err := repo.QueryByID(id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if record.name != recordName {
+		log.Fatal("Record in DB (%q) doesn't match expectation (%q)\n", record.name, recordName)
+	}
+	fmt.Printf("Successfully queried record with id %d: %+v\n", id, record)
+
+	updatedName := fmt.Sprintf("record-%d", time.Now().Unix())
+	if err := repo.Update(id, updatedName); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Updated record with id %d\n", id)
+
+	record, err = repo.QueryByID(id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if record.name != updatedName {
+		log.Fatal("Record in DB (%q) doesn't match expectation (%q)\n", record.name, updatedName)
+	}
+	fmt.Printf("Successfully queried record with id %d: %+v\n", id, record)
+
+	if err := repo.Delete(id); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Deleted record with id %d\n", id)
+
+	record, err = repo.QueryByID(id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if record != nil {
+		fmt.Printf("Expected no record with %d anymore, but it still exists: %q\n", id, record)
+	}
+	fmt.Printf("Verified deletion of record with id %d\n", id)
+
+	fmt.Printf("End of demonstration.\n")
+}
